99bb4f6abf93266213ecb6ea7da7edd45c2d52c4
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-08-14 13:00:30 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+
+
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const morgan = require('morgan')
+const connectDB = require('./config/connection')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 8080
+
+//log requests
+app.use(morgan('tiny'))
+
+//db connection
+connectDB()
+
+// load routes
+// const userRouter = require('./routes/users')
+// const taskRouter = require('./routes/tasks')
+
+// app.use('/api/users', userRouter)
+// app.use('/api/tasks', taskRouter)
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port:${PORT}`)
+})
